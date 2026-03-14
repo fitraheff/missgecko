fbe013865bbd0609ad3e9105a3916e824a135f90
fitraheff <mhmdfitrah24@gmail.com>
2026-03-14 15:11:55 +0700

    add supabase

--- prisma.config.ts
+++ prisma.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     seed: 'ts-node prisma/seed.ts',
   },
   datasource: {
-    url: process.env['DATABASE_URL'],
+    // url: process.env['DATABASE_URL'],
+    url: process.env['DIRECT_URL'],
   },
 });
